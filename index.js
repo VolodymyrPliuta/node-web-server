a389a85ef07cda678a00647d7b058fc19bd0a117
Americanman <snowboardracer7@gmail.com>
2020-03-29 13:10:49 -0400

    Add custom module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 var http = require('http');
+var time = require('./modules/time');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! ');
+  res.end('Hello World!, your time is' + JSON.stringify(time.myDateTime()) );
 }).listen(8080);
