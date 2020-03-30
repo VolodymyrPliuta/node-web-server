7cba56fca7b3292ba39f360e4423d905293b69b8
Americanman <snowboardracer7@gmail.com>
2020-03-30 02:25:49 -0400

    Serve/Read file accoring to pathname

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 
-var http = require('http');
 var time = require('./modules/time');
 
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('This is your query params look like, ofcourse if had it in url ' + req.url)
-  // res.end('Hello World!, your time is' + JSON.stringify(time.myDateTime()) );
+  // parsing url using one of built-in modules(url)
+  var q = url.parse(req.url, true);
+  // we are using q.parhname method for this scenario as well methods like q.serch(returns '?year=2017&month=february')
+  // q.query(returns an object: { year: 2017, month: 'february' }), q.host
+  var filename = "./html" + q.pathname;
+  console.log(filename)
+  fs.readFile(filename + ".html", function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found, try to hit valid url for example localhost:8080/summer");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
